31528dc8e931dc135eda3af7f4382bf75c10c654
Samin1615088 <samin.shafkat@gmail.com>
2021-04-27 20:09:15 +0600

    redux-toolkit-duck-pattern branch added

--- src/store/storeAppConfigure.js
+++ src/store/storeAppConfigure.js
@@ -2,8 +2,6 @@ import { createStore } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./bugs"
 
-
-
 export default function configureStore() {
     const store = createStore(reducer, devToolsEnhancer({ trace: true }));
     return store;
