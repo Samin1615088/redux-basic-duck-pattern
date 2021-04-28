62e7ba270180b44816d75fe3df8d253cc2aa348c
Samin1615088 <samin.shafkat@gmail.com>
2021-04-28 10:35:17 +0600

    create-slice added without previous code comment

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -6,10 +6,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 //actionCreator>
-// export const createBug = createAction('BUG_ADDED');
-// export const removeBug = createAction('BUG_REMOVED');
-// export const resolveBug = createAction('BUG_RESOLVED');
-// export const updateBug = createAction('BUG_UPDATED');
+
 //actionCreator<
 
 
@@ -47,31 +44,4 @@ const { actions, reducer } = bugSlice
 export const { createBug, removeBug, resolveBug, updateBug } = actions
 // Export the reducer, either as a default or named export
 export default reducer
-
-// export default createReducer([], {
-//     //key: value 
-//     //actions: functions (event => event handler)   --> just for understanding
-//     [createBug.type]: (state, action) => {    //BUG_ADDED: (state, action) => {
-//         state.push({
-//             id: ++lastId,
-//             description: action.payload.description,
-//             resolved: false
-//         })
-//     },
-
-//     [removeBug.type]: (state, action) => {  //BUG_REMOVED: (state, action) => {
-//         return state.filter(bug => bug.id !== action.payload.id);
-//     },
-
-//     [resolveBug.type]: (state, action) => {    //BUG_RESOLVED: (state, action) => {
-//         const index = state.findIndex(bug => bug.id === action.payload.id);
-//         state[index].resolved = true;
-//     },
-
-//     [updateBug.type]: (state, action) => {  //BUG_UPDATED: (state, action) => {
-//         const index = state.findIndex(bug => bug.id === action.payload.id);
-//         state[index].description = `${state[index].description} ${action.payload.description}`
-//     }
-
-// })
 //reducer<
