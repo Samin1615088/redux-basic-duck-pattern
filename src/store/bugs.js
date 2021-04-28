683013691b8ef0744b8de7fcf20843677d8af8de
Samin1615088 <samin.shafkat@gmail.com>
2021-04-28 10:33:18 +0600

    create-slice added with previous code comment

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 //actionTypes>
 ////no need for actionType as we used ex: "BUG_ADDED" is used only in one place. 
@@ -6,39 +6,72 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 
 
 //actionCreator>
-export const createBug = createAction('BUG_ADDED');
-export const removeBug = createAction('BUG_REMOVED');
-export const resolveBug = createAction('BUG_RESOLVED');
-export const updateBug = createAction('BUG_UPDATED');
+// export const createBug = createAction('BUG_ADDED');
+// export const removeBug = createAction('BUG_REMOVED');
+// export const resolveBug = createAction('BUG_RESOLVED');
+// export const updateBug = createAction('BUG_UPDATED');
 //actionCreator<
 
 
 //reducer>
 let lastId = 0;
-export default createReducer([], {
-    //key: value 
-    //actions: functions (event => event handler)   --> just for understanding
-    [createBug.type]: (state, action) => {    //BUG_ADDED: (state, action) => {
-        state.push({
-            id: ++lastId,
-            description: action.payload.description,
-            resolved: false
-        })
+//createSlice function that will auto-generate the action types and action creators for yme
+const bugSlice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        createBug(state, action) {
+            state.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            })
+        },
+        removeBug(state, action) {
+            return state.filter(bug => bug.id !== action.payload.id);
+        },
+        resolveBug(state, action) {
+            const index = state.findIndex(bug => bug.id === action.payload.id);
+            state[index].resolved = true;
+        },
+        updateBug(state, action) {
+            const index = state.findIndex(bug => bug.id === action.payload.id);
+            state[index].description = `${state[index].description} ${action.payload.description}`
+        }
     },
-
-    [removeBug.type]: (state, action) => {  //BUG_REMOVED: (state, action) => {
-        return state.filter(bug => bug.id !== action.payload.id);
-    },
-
-    [resolveBug.type]: (state, action) => {    //BUG_RESOLVED: (state, action) => {
-        const index = state.findIndex(bug => bug.id === action.payload.id);
-        state[index].resolved = true;
-    },
-
-    [updateBug.type]: (state, action) => {  //BUG_UPDATED: (state, action) => {
-        const index = state.findIndex(bug => bug.id === action.payload.id);
-        state[index].description = `${state[index].description} ${action.payload.description}`
-    }
-
 })
+
+// Extract the action creators object and the reducer
+const { actions, reducer } = bugSlice
+// Extract and export each action creator by name
+export const { createBug, removeBug, resolveBug, updateBug } = actions
+// Export the reducer, either as a default or named export
+export default reducer
+
+// export default createReducer([], {
+//     //key: value 
+//     //actions: functions (event => event handler)   --> just for understanding
+//     [createBug.type]: (state, action) => {    //BUG_ADDED: (state, action) => {
+//         state.push({
+//             id: ++lastId,
+//             description: action.payload.description,
+//             resolved: false
+//         })
+//     },
+
+//     [removeBug.type]: (state, action) => {  //BUG_REMOVED: (state, action) => {
+//         return state.filter(bug => bug.id !== action.payload.id);
+//     },
+
+//     [resolveBug.type]: (state, action) => {    //BUG_RESOLVED: (state, action) => {
+//         const index = state.findIndex(bug => bug.id === action.payload.id);
+//         state[index].resolved = true;
+//     },
+
+//     [updateBug.type]: (state, action) => {  //BUG_UPDATED: (state, action) => {
+//         const index = state.findIndex(bug => bug.id === action.payload.id);
+//         state[index].description = `${state[index].description} ${action.payload.description}`
+//     }
+
+// })
 //reducer<
